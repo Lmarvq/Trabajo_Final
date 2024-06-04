34e59fbc1a891335ed3179833ef21c4e7bef9d7f
Daniu13 <daniel.lozano1@udea.edu.co>
2024-06-04 16:06:49 -0500

    Hola

--- modeloPOO.py
+++ modeloPOO.py
@@ -3,8 +3,11 @@ import mysql.connector.cursor
 import numpy as np
 import scipy.io as sio
 
+# Hola
+
+
 class DataAccesObject:
-    def __init__(self, data = None) -> None:
+    def __init__(self, data=None) -> None:
         self.__host = ""
         self.__usuario = ""
         self.__password = ""
@@ -17,26 +20,24 @@ class DataAccesObject:
             self.canales = 0
             self.puntos = 0
 
-    def asignarDatos(self,data):
-        self.data = data # Matriz 2D
-        self.canales = data.shape[0] #8 canales 
-        self.puntos = data.shape[1]  #360000 puntos  
+    def asignarDatos(self, data):
+        self.data = data  # Matriz 2D
+        self.canales = data.shape[0]  # 8 canales
+        self.puntos = data.shape[1]  # 360000 puntos
 
     def devolver_segmento(self, x_min, x_max):
         if x_min >= x_max:
             return None
-        return self.data[:,x_min:x_max]
+        return self.data[:, x_min:x_max]
 
-    def escalar_senal(self,x_min,x_max, escala):
+    def escalar_senal(self, x_min, x_max, escala):
         if x_min >= x_max:
             return None
-        copia_data = self.data[:,x_min:x_max].copy()
-        return copia_data*escala
+        copia_data = self.data[:, x_min:x_max].copy()
+        return copia_data * escala
 
-    def promedio(self,c,xmax,xmin):
-        return np.mean(self.data[c, xmin:xmax],0)
-    
-    
+    def promedio(self, c, xmax, xmin):
+        return np.mean(self.data[c, xmin:xmax], 0)
 
     def set_parametros_conexion(self, host, usuario, password):
         self.__host = host
@@ -46,9 +47,7 @@ class DataAccesObject:
     def conectarDB(self):
         try:
             self.conexion = mysql.connector.connect(
-                host=self.__host,
-                user=self.__usuario,
-                password=self.__password
+                host=self.__host, user=self.__usuario, password=self.__password
             )
             self.conexion.cursor().execute("USE PruebaLOGIN")
             return True
@@ -66,34 +65,42 @@ class DataAccesObject:
             try:
                 cursor.execute("CREATE DATABASE IF NOT EXISTS PruebaLOGIN")
                 cursor.execute("USE PruebaLOGIN")
-                cursor.execute("""
+                cursor.execute(
+                    """
                     CREATE TABLE IF NOT EXISTS Usuarios (
                         ID int AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
                         Usuario VARCHAR(60) UNIQUE,
                         Password VARCHAR(60)
                     )
-                """)
-                cursor.execute("""
+                """
+                )
+                cursor.execute(
+                    """
                     CREATE TABLE IF NOT EXISTS Pacientes (
                         Nombre VARCHAR(60),
                         Cedula int PRIMARY KEY,
                         Ruta VARCHAR(200)
                     )
-                """)
-                cursor.execute("""
+                """
+                )
+                cursor.execute(
+                    """
                     CREATE TABLE IF NOT EXISTS Imagenes (
                         ID_cedula int PRIMARY KEY,
                         FOREIGN KEY(ID_Cedula) REFERENCES Pacientes(Cedula),
                         Ruta VARCHAR(200)
                     )
-                """)
-                cursor.execute("""
+                """
+                )
+                cursor.execute(
+                    """
                     CREATE TABLE IF NOT EXISTS Biosenales (
                         ID_Cedula int PRIMARY KEY,
                         FOREIGN KEY(ID_Cedula) REFERENCES Pacientes(Cedula),
                         Ruta VARCHAR(200)
                     )
-                """)
+                """
+                )
                 self.conexion.commit()
                 return True
             except mysql.connector.Error as err:
@@ -108,10 +115,10 @@ class DataAccesObject:
         if self.conectarDB():
             cursor = self.conexion.cursor()
             try:
-                query = 'INSERT INTO Usuarios (Usuario, Password) VALUES (%s, %s)'
+                query = "INSERT INTO Usuarios (Usuario, Password) VALUES (%s, %s)"
                 cursor.execute(query, (u, p))
                 self.conexion.commit()
-                #cursor.execute("""GRANT ALL PRIVILEGES ON DB* TO %s@'localhost' WITH GRANT OPTION;""", tupla)
+                # cursor.execute("""GRANT ALL PRIVILEGES ON DB* TO %s@'localhost' WITH GRANT OPTION;""", tupla)
                 return True
             except mysql.connector.Error as err:
                 print(f"Error: {err}")
@@ -125,7 +132,7 @@ class DataAccesObject:
         if self.conectarDB():
             cursor = self.conexion.cursor()
             try:
-                query = 'SELECT * FROM Usuarios WHERE Usuario = %s AND Password = %s'
+                query = "SELECT * FROM Usuarios WHERE Usuario = %s AND Password = %s"
                 cursor.execute(query, (u, p))
                 if cursor.fetchone():
                     return True
@@ -137,9 +144,12 @@ class DataAccesObject:
                 cursor.close()
                 self.cerrarDB()
         return False
+
     def obtener_pacientes(self):
         if self.conectarDB():
-            cursor = self.conexion.cursor(dictionary=True)  # Esto hará que el cursor devuelva los resultados como diccionarios
+            cursor = self.conexion.cursor(
+                dictionary=True
+            )  # Esto hará que el cursor devuelva los resultados como diccionarios
             try:
                 cursor.execute("SELECT * FROM Pacientes")
                 resultados = cursor.fetchall()
@@ -151,12 +161,16 @@ class DataAccesObject:
                 cursor.close()
                 self.cerrarDB()
         return []
+
     def agregar_paciente(self, nombre, cedula, ruta):
         if self.conectarDB():
             cursor = self.conexion.cursor()
             try:
                 cursor.execute("USE PruebaLOGIN")
-                cursor.execute("INSERT INTO Pacientes (Nombre, Cedula, Ruta) VALUES (%s, %s, %s)", (nombre, cedula, ruta))
+                cursor.execute(
+                    "INSERT INTO Pacientes (Nombre, Cedula, Ruta) VALUES (%s, %s, %s)",
+                    (nombre, cedula, ruta),
+                )
                 self.conexion.commit()
                 return True
             except mysql.connector.Error as err:
@@ -166,12 +180,15 @@ class DataAccesObject:
                 cursor.close()
                 self.cerrarDB()
         return False
+
     def eliminar_paciente(self, cedula_paciente):
         if self.conectarDB():
             cursor = self.conexion.cursor()
             try:
                 cursor.execute("USE PruebaLOGIN")
-                cursor.execute("DELETE FROM Pacientes WHERE Cedula = %s", (cedula_paciente,))
+                cursor.execute(
+                    "DELETE FROM Pacientes WHERE Cedula = %s", (cedula_paciente,)
+                )
                 self.conexion.commit()
                 return True
             except mysql.connector.Error as err:
@@ -181,12 +198,16 @@ class DataAccesObject:
                 cursor.close()
                 self.cerrarDB()
         return False
+
     def editar_paciente(self, nombre, ruta, cedula):
         if self.conectarDB():
             cursor = self.conexion.cursor()
             try:
                 cursor.execute("USE PruebaLOGIN")
-                cursor.execute("UPDATE Pacientes SET Nombre = %s, Ruta = %s WHERE Cedula = %s", (nombre, ruta, cedula))
+                cursor.execute(
+                    "UPDATE Pacientes SET Nombre = %s, Ruta = %s WHERE Cedula = %s",
+                    (nombre, ruta, cedula),
+                )
                 self.conexion.commit()
                 return True
             except mysql.connector.Error as err:
@@ -196,7 +217,7 @@ class DataAccesObject:
                 cursor.close()
                 self.cerrarDB()
         return False
-    
+
     def obtener_medicamentos(self, cedula):
         if self.conectarDB():
             cursor = self.conexion.cursor(dictionary=True)
@@ -214,26 +235,26 @@ class DataAccesObject:
             cursor.close()
             self.cerrarDB()
             return resultado
-            
+
 
 # class Paciente:
 #     def __init__(self, dao):
 #         self.dao = dao
-#         self.__nombre="" 
+#         self.__nombre=""
 #         self.__cedula=0
 #         self.__biosignal = self.cargar_biosenal
 
 #     def AsignarNombre(self, n):
-#         self.__nombre = n 
+#         self.__nombre = n
 
 #     def AsignarCedula(self, c):
-#         self.__cedula = c 
+#         self.__cedula = c
 
 #     def VerNombre(self):
-#         return self.__nombre 
+#         return self.__nombre
 
 #     def VerCedula(self):
-#         return self.__cedula 
+#         return self.__cedula
 
 #     def cargar_biosenal(self):
 #         biosenal_data = self.dao.obtener_biosenal(self.__cedula)
@@ -251,11 +272,11 @@ class DataAccesObject:
 #             self.data = []
 #             self.canales = 0
 #             self.puntos = 0
-    
+
 #     def asignarDatos(self,data):
 #         self.data = data # Matriz 2D
-#         self.canales = data.shape[0] #8 canales 
-#         self.puntos = data.shape[1]  #360000 puntos  
+#         self.canales = data.shape[0] #8 canales
+#         self.puntos = data.shape[1]  #360000 puntos
 
 #     def devolver_segmento(self, x_min, x_max):
 #         if x_min >= x_max:
